9474f5ddaede2ea4b8889083b7bc3a3f2eca4e4d
AjrinNR <ajrinnr11@gmail.com>
2020-01-22 11:47:58 +0700

    Laravel next Level

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,62 @@ Route::get('biodata/{nama}/{alamat}/{sekolah}', function($a,$b,$c){
             '<br>Alamat saya di '.$b.
             '<br>Sekolah di '.$c;
 });
+
+Route::get('pesan/{makan?}/{minum?}/{harga?}', function ($m = null,$n = null, $h=null) {
+    $a = 'Silahkan Pesan terlebih dahulu';
+    if (isset($m) ) {
+        $a = 'Anda membeli ' . $m ;
+    }
+    if (isset($n)) {
+        $a .= ' dan '.$n ;
+    }
+    if (isset($h)) {
+
+        if($h<0){
+            $f = 'harga tidak valid';
+        }
+        elseif ($h>=35000) {
+            $f = 'Size Large';
+        }elseif ($h>=25000) {
+            $f = 'Size Medium';
+        }else{
+            $f = 'Size Small';
+
+        }
+        $a.=' dengan harga '. $h .' dan '.$f;
+    }
+    return $a;
+});
+Route::get('testtni/{nama?}/{bb?}/{umur?}', function($n=null,$b=null,$u=null){
+    $a = 'Silahkan isi terlebih dahulu';
+    if (isset($n)) {
+        $a = 'Nama anda : '.$n.'<br>';
+    }
+    if (isset($b)) {
+        if ($b>= 76 && $b<=100) {
+            $f = 'Anda harus diturunkan';
+        }
+        elseif ($b>=65 && $b<=75) {
+            $f = 'Berat anda ideal';
+        }
+        elseif ($b>=50 && $b<=64) {
+            $f = 'Anda harus dinaikan ';
+        }elseif ($b<50) {
+            $f = 'Anda kurang nutrisi';
+        }
+        $a.= 'Berat Badan '.$f;
+    }
+    if (isset($u)) {
+        if ($u>= 30 && $u<=40) {
+            $j = 'Perwira';
+        }
+        elseif ($u>=40 && $u<=50) {
+            $j = 'Laksamana';
+        }
+        elseif ($u>=50 && $u<=60) {
+            $j = 'Jendral';
+        }
+        $a.='<br>Pangkat Anda :'.$j;
+    }
+    return $a;
+});
